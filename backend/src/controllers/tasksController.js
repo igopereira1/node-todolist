7478bd2255eb2f0aad86e70dcc1439c7c4186687
igopereira1 <igopereira1@gmail.com>
2024-02-17 20:57:16 -0300

    feat(tasks): add updateTask function to controller

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -14,10 +14,17 @@ const deleteTask = async (req, res) => {
   const { id } = req.params;
   await tasksModel.deleteTask(id);
   return res.status(204).json();
-
 };
+
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  await tasksModel.updateTask(id, req.body);
+  return res.status(204).json();
+};
+
 module.exports = {
   getAllTasks,
   createTask,
-  deleteTask
+  deleteTask,
+  updateTask
 };
