08cc9952fc3cee41975cdbb1c38d6a9943496275
igopereira1 <igopereira1@gmail.com>
2024-02-17 10:37:40 -0300

    chore: update router.js and server.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const tasksController = require('./controllers/tasksController')
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.status(200).send('Hello, world!'));
+router.get('/tasks', getAllTasks)
 
 module.exports = router;
