33a71113ce1d79433f3b856f03aee7992cffed97
igopereira1 <igopereira1@gmail.com>
2024-02-18 13:38:56 -0300

    feat(router): add validation middlewares to POST and PUT routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,8 +5,17 @@ const tasksMiddleware = require('./middlewares/tasksMiddleware');
 const router = express.Router();
 
 router.get('/tasks', tasksController.getAllTasks);
-router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
+router.post(
+  '/tasks',
+  tasksMiddleware.validateTitle,
+  tasksController.createTask
+);
 router.delete('/tasks/:id', tasksController.deleteTask);
-router.put('/tasks/:id', tasksController.updateTask);
+router.put(
+  '/tasks/:id',
+  tasksMiddleware.validateTitle,
+  tasksMiddleware.validateStatus,
+  tasksController.updateTask
+);
 
 module.exports = router;
