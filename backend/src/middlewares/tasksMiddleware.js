70a3dd68929143c2febcec1083f1135b027c14b3
igopereira1 <igopereira1@gmail.com>
2024-02-18 13:35:46 -0300

    feat(middleware): add validateStatus middleware

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -1,19 +1,36 @@
-const validateBody = (req, res, next) => {
+const validateTitle = (req, res, next) => {
   const { body } = req;
-
+  
   if (body.title == undefined) {
     return res.status(400).json({ message: 'The field "title" is required!' });
   }
-
+  
   if (body.title == '') {
     return res
       .status(400)
       .json({ message: 'The field "title" cannot be empty!' });
   }
-
+  
   next();
 };
-
+  
+const validateStatus = (req, res, next) => {
+  const { body } = req;
+    
+  if (body.status == undefined) {
+    return res.status(400).json({ message: 'The field "status" is required!' });
+  }
+    
+  if (body.status == '') {
+    return res
+      .status(400)
+      .json({ message: 'The field "status" cannot be empty!' });
+  }
+    
+  next();
+};
+  
 module.exports = {
-  validateBody,
+  validateTitle,
+  validateStatus
 };
