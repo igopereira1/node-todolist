bfc7a6fe7b6dacbf46f3ae6b40d0bc1d36bef201
igopereira1 <igopereira1@gmail.com>
2024-02-18 22:19:30 -0300

    chore: configure CORS in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = require('./router');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
 app.use(express.json());
 app.use(router);
 
